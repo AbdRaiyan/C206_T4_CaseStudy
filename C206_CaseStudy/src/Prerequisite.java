4d66c71b7423ad25d7f6d703772c855c519bcfca
AbdRaiyan <20022280@myrp.edu.sg>
2022-08-05 09:18:44 +0800

    Ite-1

--- C206_CaseStudy/src/Prerequisite.java
+++ C206_CaseStudy/src/Prerequisite.java
@@ -1,4 +1,14 @@
 
 public class Prerequisite {
-
+	
+int preid = 0;
+int subid = 0;
+String desc = "";
+	
+	public Prerequisite(int preid, int subid, String desc) {
+		preid = this.preid;
+		subid = this.subid;
+		desc = this.desc;
+	}
+	
 }
