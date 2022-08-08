bcbd174e2d5b80412603a2ce341946c336336ead
Kai <21024750@myrp.edu.sg>
2022-08-08 11:21:36 +0800

    draft_user_login

--- C206_CaseStudy/src/User_Account.java
+++ C206_CaseStudy/src/User_Account.java
@@ -1,6 +1,30 @@
 
-public class User_Account {
-
+import java.util.Scanner;
+public class User_Account { 
 	
 
-}
+	   public static void main(String[] args) 
+	   {
+
+	     public UserLogin(String username, String password) {
+	    	 this.username = username;
+	    	 this.password = password;
+	     }
+
+
+	     System.out.println("Enter your username and password to login to your account.");    
+	     System.out.println("Username: ");
+
+	     System.out.println("Password: ");
+
+	       UserLogin login = new UserLogin(username, password);
+
+	       if(login.checkPassword())
+	           System.out.println("Login successful");
+	       else
+	           System.out.println("The username or password you entered is incorrect.");
+	   }
+
+
+	}
+
