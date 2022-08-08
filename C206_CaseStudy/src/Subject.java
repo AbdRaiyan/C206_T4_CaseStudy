b431523b6d38d0b51f6c43d3a0d10ffaa454b531
Shakir <21018545@rp.edu.sg>
2022-08-08 12:13:17 +0800

    commit

--- C206_CaseStudy/src/Subject.java
+++ C206_CaseStudy/src/Subject.java
@@ -1,18 +1,21 @@
 public class Subject{
 	
 	public addSubject{
+		
 		if addSubject == True:
 			System.out.println("Subject added");
 		else: 
 			System.out.println("Subject not addedd");
 	}
     public deleteSubject{
+    	
     	if  deleteSubject == True:
     		System.out.println("Subject deleted");
 		else: 
   			System.out.println("Subject not deleted");
     }
     public editSubject{
+    	
     	if editSubject == True:
     		System.out.println("Subject edited");
 		else: 
