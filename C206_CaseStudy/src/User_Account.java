d34fa66f87b080ac0f40c0ad3ef0a4b1d7503466
Kai <21024750@myrp.edu.sg>
2022-08-08 11:21:53 +0800

    draft userlogin

--- C206_CaseStudy/src/User_Account.java
+++ C206_CaseStudy/src/User_Account.java
@@ -1,9 +1,5 @@
-
-import java.util.Scanner;
 public class User_Account { 
-	
-
-	   public static void main(String[] args) 
+	 public static void main(String[] args) 
 	   {
 
 	     public UserLogin(String username, String password) {
