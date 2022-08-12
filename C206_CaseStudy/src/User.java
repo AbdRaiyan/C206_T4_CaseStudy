e2afa1f9a136bc9913e034966f9c4ac54b1ec224
21037552 <21037552@DESKTOP-1BBOGZ9T>
2022-08-13 00:17:25 +0800

    Did career class code & a bit of User class, Import Helper class

---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,16 @@
+public class User { 
+	private String name;// done by Gerlad
+	private String role;// done by Gerlad
+	public User(String name, String role) {// done by Gerlad
+		super();
+		this.name = name;
+		this.role = role;
+	}
+	public String getName() {// done by Gerlad
+		return name;
+	}
+	public String getRole() {// done by Gerlad
+		return role;
+	}
+
+}
---
+++ C206_CaseStudy/src/career.java
@@ -0,0 +1,21 @@
+public class career { // done by gerald
+	private int id;
+	private String name;
+	public career(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
